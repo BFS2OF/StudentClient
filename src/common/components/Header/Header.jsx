807c7a0943c3b665e3a903d913047b28a1a5c37b
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-14 21:22:10 +0200

    Created the Header.jsx

---
+++ src/common/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import "./styles.sass";
+import BS2Image from "@/common/images/bs2ab.png";
+import Button from "@/common/components/Button";
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <img src={BS2Image} alt="BS2" />
+                <h1 className="logo-text">Fragenportal</h1>
+            </div>
+
+            <Button text="MTLM Product" />
+        </header>
+    )
+}
