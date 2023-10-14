3272e70e11f084e7b5b4e8e237dab82c516349de
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-15 00:03:00 +0200

    Added the onClick listener to the Button.jsx

--- src/common/components/Button/Button.jsx
+++ src/common/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import "./styles.sass";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-export const Button = ({text, icon}) => {
+export const Button = ({text, icon, onClick}) => {
     return (
-        <button className="btn">
+        <button className="btn" onClick={onClick}>
             {icon && <FontAwesomeIcon icon={icon} />}
             {text}
         </button>
