5434652792c34c9bb59e4bb96e8575d1fa7b2233
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-15 00:14:16 +0200

    Implemented a state system in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import "@/common/styles/main.sass";
 import "@/common/styles/fonts.sass";
 import Header from "@/common/components/Header";
+import EnterRoom from "@/pages/EnterRoom";
+import LoadingBar from "@/common/components/LoadingBar";
+import NameChooser from "@/pages/NameChooser";
+import {useState} from "react";
+import {socket} from "@/common/utils/socket.js";
 
-const App = () => (
-    <>
-        <Header />
-    </>
-);
+const App = () => {
+    const [state, setState] = useState("enter");
+    const [code, setCode] = useState(null);
+
+    socket.connect();
+
+    return (
+        <>
+            <Header/>
+            <main>
+                {state === "enter" && <EnterRoom setState={setState} socket={socket} code={code} setCode={setCode}/>}
+                {state === "name" && <NameChooser setState={setState} socket={socket} code={code}/>}
+            </main>
+            <LoadingBar progress={100}/>
+        </>
+    );
+}
 
 export default App;
