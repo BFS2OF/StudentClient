148840b50842f904d6ed6737c05a6e4ccd7c5b24
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-14 21:22:25 +0200

    Integrated the Header into the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "@/common/styles/main.sass";
 import "@/common/styles/fonts.sass";
+import Header from "@/common/components/Header";
 
-const App = () => {
+const App = () => (
+    <>
+        <Header />
+    </>
+);
 
-  return (
-    <></>
-  )
-}
-
-export default App
+export default App;
