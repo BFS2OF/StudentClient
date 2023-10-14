e3139b1cf81a543708b862eac4f38e6211457bc1
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-15 00:26:26 +0200

    Updated the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import Header from "@/common/components/Header";
 import EnterRoom from "@/pages/EnterRoom";
 import LoadingBar from "@/common/components/LoadingBar";
 import NameChooser from "@/pages/NameChooser";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {socket} from "@/common/utils/socket.js";
 
 const App = () => {
     const [state, setState] = useState("enter");
     const [code, setCode] = useState(null);
 
-    socket.connect();
+    useEffect(() => {
+        socket.connect();
+
+        return () => {
+            socket.disconnect();
+        }
+    }, []);
 
     return (
         <>
