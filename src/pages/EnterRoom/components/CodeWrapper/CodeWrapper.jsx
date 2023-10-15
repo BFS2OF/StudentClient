267a14d2931f844c5c5c6850f93b63887fed166d
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-15 14:23:51 +0200

    Updated the CodeWrapper.jsx

--- src/pages/EnterRoom/components/CodeWrapper/CodeWrapper.jsx
+++ src/pages/EnterRoom/components/CodeWrapper/CodeWrapper.jsx
@@ -1,13 +1,28 @@
 import "./styles.sass";
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 
-export const CodeWrapper = ({onChange}) => {
+export const CodeWrapper = ({onChange, resetCode}) => {
 
     const codeWrapper = useRef(null);
 
+    useEffect(() => {
+        codeWrapper.current.childNodes.forEach(input => input.value = "");
+        codeWrapper.current.childNodes[0].focus();
+    }, [resetCode]);
+
+    useEffect(() => {
+        codeWrapper.current.addEventListener("keydown", (e) => {
+            if (e.key !== "Backspace") return;
+
+            codeWrapper.current.childNodes.forEach((input, index) => {
+                if (e.target === input && index !== 0) codeWrapper.current.childNodes[index - 1].focus();
+            });
+        });
+    }, []);
+
     const handleChange = (e) => {
         codeWrapper.current.childNodes.forEach((input, index) => {
-            if (e.target === input && index !== 3) {
+            if (e.target === input && e.target.value.length === 1 && index !== 3) {
                 codeWrapper.current.childNodes[index + 1].focus();
             } else if (e.target === input && index === 3) {
                 let code = "";
@@ -21,7 +36,8 @@ export const CodeWrapper = ({onChange}) => {
 
     return (
         <div className="code-wrapper" ref={codeWrapper}>
-            {[...Array(4)].map((_, index) => <input key={index} type="number" placeholder="0" onChange={handleChange} />)}
+            {[...Array(4)].map((_, index) => <input key={index} type="number" placeholder="0"
+                                                    onChange={handleChange}/>)}
         </div>
     )
 }
