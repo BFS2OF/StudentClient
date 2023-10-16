ff17ac6cd16f58e27345e8f079e84765c336231b
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-16 22:21:39 +0200

    Updated the Loading.jsx

--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -1,6 +1,20 @@
 import "./styles.sass";
+import {useEffect} from "react";
 
-export const Loading = () => {
+export const Loading = ({setState, socket}) => {
+
+    useEffect(() => {
+        const handler = (data) => {
+            setState("in-game");
+        }
+
+        socket.on("QUESTION_RECEIVED", handler);
+
+        return () => {
+            socket.off("QUESTION_RECEIVED", handler);
+        }
+    }, []);
+
     return (
         <div className="loading-page">
             <div className="lds"><div /><div /><div /></div>
