cc1d3361e37fe6cc3ff10bd8fb01151be63e5049
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-16 08:00:55 +0200

    Created the Loading.jsx component

---
+++ src/pages/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+import "./styles.sass";
+
+export const Loading = () => {
+    return (
+        <div className="loading-page">
+            <div className="lds"><div /><div /><div /></div>
+        </div>
+    )
+}
