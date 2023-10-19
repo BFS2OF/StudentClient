71b353a557d0fb6654099e266b4b033758b5fa17
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-19 14:57:51 +0200

    Updated the Header.jsx

--- src/common/components/Header/Header.jsx
+++ src/common/components/Header/Header.jsx
@@ -2,15 +2,13 @@ import "./styles.sass";
 import BS2Image from "@/common/images/bs2ab.png";
 import Button from "@/common/components/Button";
 
-export const Header = ({name}) => {
-    return (
-        <header className="header">
-            <div className="logo">
-                <img src={BS2Image} alt="BS2" />
-                <h1 className="logo-text">Fragenportal</h1>
-            </div>
+export const Header = ({name}) => (
+    <header className="header">
+        <div className="logo">
+            <img src={BS2Image} alt="BS2" />
+            <h1 className="logo-text">Fragenportal</h1>
+        </div>
 
-            <Button text={name ? name : "MTLM Product"} />
-        </header>
-    )
-}
+        <Button text={name ? name : "MTLM Product"} />
+    </header>
+)
