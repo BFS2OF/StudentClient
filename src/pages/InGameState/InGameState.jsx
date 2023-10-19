c7daf7c10bf057fd52b8e777d25ff2434847aaaf
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-19 14:51:22 +0200

    Created the InGameState.jsx

--- src/pages/InGameState/InGameState.jsx
+++ src/pages/InGameState/InGameState.jsx
@@ -1,11 +1,32 @@
 import "./styles.sass";
 import {useEffect, useState} from "react";
+import Background from "@/common/components/Background";
+import SCAnswer from "@/pages/InGameState/components/SingleChoiceAnswer";
+import Question from "@/pages/InGameState/components/Question";
 
-export const InGameState = () => {
+export const InGameState = ({socket, setState, setProgress}) => {
     const [countdown, setCountdown] = useState(3);
 
+    const question = {
+        title: "Wann wurde die erste Version von Minecraft veröffentlicht?",
+        type: "SINGLE_CHOICE",
+        answers: ["2009", "2010", "2011", "Gute Frage"],
+        correctAnswer: 1
+    }
+
     useEffect(() => {
-        if (countdown === 0) return;
+        if (countdown === 0) {
+            const interval = setInterval(() => {
+                setProgress(prev => {
+                    if (prev === 0) clearInterval(interval);
+
+                    return prev - 10;
+                });
+            }, 1000);
+            return () => clearInterval(interval);
+        }
+        setProgress(100);
+
         const interval = setInterval(() => {
             setCountdown(prev => {
                 if (prev === 1) clearInterval(interval);
@@ -15,24 +36,27 @@ export const InGameState = () => {
         }, 1000);
 
         return () => clearInterval(interval);
-    }, []);
+    }, [countdown]);
 
     return (
-        <>
+        <div className="game-page">
             {countdown !== 0 && <div className="countdown-page">
                 <div className="countdown">
                     <h1>{countdown}</h1>
-                </div>
-                <div className="countdown-circle">
+                    <div className="countdown-circle" />
+                    <div className="background"></div>
                 </div>
             </div>}
             {countdown === 0 && <div className="game-page">
-                <div className="game">
-                    <h1>ingame state</h1>
-                    <p>questions WIP</p>
+                <Question title={question.title} />
+
+                <div className="answers-container">
+                    {question.answers.map((answer, index) => (
+                        <SCAnswer answer={answer} key={index} onClick={() => setCountdown(5000)} />
+                    ))}
                 </div>
             </div>}
-
-        </>
+            <Background />
+        </div>
     );
 }
