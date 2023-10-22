c5440e5a2598793d311c925b3b27ecfd0ddf612f
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-22 20:07:14 +0200

    Removed the QUESTION_RECEIVED hook from the Loading.jsx

--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -1,19 +1,6 @@
 import "./styles.sass";
-import {useEffect} from "react";
 
 export const Loading = ({setState, socket}) => {
-
-    useEffect(() => {
-        const handler = (data) => {
-            setState("in-game");
-        }
-
-        socket.on("QUESTION_RECEIVED", handler);
-
-        return () => {
-            socket.off("QUESTION_RECEIVED", handler);
-        }
-    }, []);
 
     return (
         <div className="loading-page">
