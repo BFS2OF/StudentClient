9ceb2c855446ece8ed8bcde555e1e6b0cf8586a8
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-23 15:21:20 +0200

    Updated the Ending.jsx

--- src/pages/Ending/Ending.jsx
+++ src/pages/Ending/Ending.jsx
@@ -1,11 +1,20 @@
 import "./styles.sass";
+import Button from "@/common/components/Button";
+import {faArrowLeftRotate} from "@fortawesome/free-solid-svg-icons";
 
-export const Ending = () => {
+export const Ending = ({setState, onRetry}) => {
+    const newGame = () => {
+        setState("enter");
+        onRetry();
+    }
     return (
         <div className="ending">
             <div className="ending-content">
                 <h1>Raum geschlossen</h1>
                 <p>Danke fürs spielen!</p>
+            </div>
+            <div className="ending-actions">
+                <Button onClick={newGame} text="Zurück zum Start" icon={faArrowLeftRotate} />
             </div>
         </div>
     );
