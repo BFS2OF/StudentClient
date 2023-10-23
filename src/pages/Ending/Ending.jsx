9cc60fba26e0cfb544b2535de9a2419b9b2b0219
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-23 15:07:08 +0200

    Created the Ending.jsx

---
+++ src/pages/Ending/Ending.jsx
@@ -0,0 +1,12 @@
+import "./styles.sass";
+
+export const Ending = () => {
+    return (
+        <div className="ending">
+            <div className="ending-content">
+                <h1>Raum geschlossen</h1>
+                <p>Danke fürs spielen!</p>
+            </div>
+        </div>
+    );
+}
