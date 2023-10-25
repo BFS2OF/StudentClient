f70b8f2b3fc2e945b313d22ec56ba599551d734a
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-25 17:15:45 +0200

    Integrated a new progress animation into the LoadingBar.jsx

--- src/common/components/LoadingBar/LoadingBar.jsx
+++ src/common/components/LoadingBar/LoadingBar.jsx
@@ -1,7 +1,17 @@
 import "./styles.sass";
+import {useEffect, useState} from "react";
 
 export const LoadingBar = ({progress = 100}) => {
+    const [transition, setTransition] = useState(true);
+
+    useEffect(() => {
+        if (progress !== 100 && transition) return;
+        setTransition(false);
+        const timeout = setTimeout(() => setTransition(true), 10000);
+        return () => clearTimeout(timeout);
+    }, [progress]);
+
     return (
-        <div className="loading-bar" style={{width: `${progress}%`}} />
+        <div className={"loading-bar" + (transition ? "" : " progress-transition-off")} style={{width: `${progress}%`}} />
     );
 }
