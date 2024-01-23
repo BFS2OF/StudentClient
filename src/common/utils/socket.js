8a5ad37f8c10be97f960e5bd36b8e5caec529f6e
Mathias Wagner <germannewsmaker@gmail.com>
2024-01-23 13:06:58 +0100

    Updated the socket.js

--- src/common/utils/socket.js
+++ src/common/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === "production" ? undefined : undefined;
+const URL = process.env.NODE_ENV === "production" ? "https://openday.gnmyt.dev/" : undefined;
 
 export const socket = io(URL);
