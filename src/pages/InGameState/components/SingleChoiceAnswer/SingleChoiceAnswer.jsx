26cdd3eace68137af2d20ab03692eeecd4d5625c
Mathias Wagner <germannewsmaker@gmail.com>
2024-02-05 06:56:09 +0100

    Fixed bugs in the InGameState components

--- src/pages/InGameState/components/SingleChoiceAnswer/SingleChoiceAnswer.jsx
+++ src/pages/InGameState/components/SingleChoiceAnswer/SingleChoiceAnswer.jsx
@@ -5,12 +5,13 @@ export const SingleChoiceAnswer = ({answer, index, onClick}) => {
 
     const [clicked, setClicked] = useState(false);
 
-    const handleAnimationEnd = (event) => {
-        if (event.animationName === "scaleOut") onClick(index+1);
+    const click = () => {
+        setClicked(true);
+        onClick(index+1);
     }
 
     return (
-        <div className={"answer" + (clicked ? " answer-clicked" : "")} onClick={() => setClicked(true)} onAnimationEnd={handleAnimationEnd}>
+        <div className={"answer" + (clicked ? " answer-clicked" : "")} onClick={() => click()}>
             <h1>{answer}</h1>
         </div>
     )
--- src/pages/InGameState/components/YesNoAnswer/YesNoAnswer.jsx
+++ src/pages/InGameState/components/YesNoAnswer/YesNoAnswer.jsx
@@ -1,18 +1,19 @@
 import "./styles.sass";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCircleCheck, faCircleXmark, faVoteYea} from "@fortawesome/free-solid-svg-icons";
+import {faCircleCheck, faCircleXmark} from "@fortawesome/free-solid-svg-icons";
 import {useState} from "react";
 
 export const YesNoAnswer = ({yes, onClick}) => {
     const [clicked, setClicked] = useState(false);
 
-    const handleAnimationEnd = (event) => {
-        if (event.animationName === "fadeOutYN") onClick(yes ? 1 : 0);
+    const click = () => {
+        setClicked(true);
+        onClick(yes ? 1 : 0);
     }
 
     return (
         <div className={"yn-answer" + (yes ? " yn-yes" : " yn-no") + (clicked ? " yn-invisible" : "")}
-             onClick={() => setClicked(true)} onAnimationEnd={handleAnimationEnd}>
+             onClick={() => click()}>
             <FontAwesomeIcon icon={yes ? faCircleCheck : faCircleXmark} />
             <h1>{yes ? "Ja" : "Nein"}</h1>
         </div>
